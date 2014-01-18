938630c664154ef3980f25c556433dae03f659ce
Joshua Davison <josh@stringbot.com>
2014-01-18 17:48:41 -0600

    clean up and docs in fm-ops

--- src/sbot_lib/fm_ops.clj
+++ src/sbot_lib/fm_ops.clj
@@ -3,14 +3,17 @@
    :require [sbot-lib.reverb :as verb]))
 
 (defn amp-sine [hz amp]
+  "scales a sine oscillator with the specified hz by amp"
   (let [* #'overtone.sc.ugen-collide/*]
     (* (sin-osc hz) amp)))
 
 (defn fm-op [hz1 amp1 hz2 amp2]
+  "use an amp-sine to modulate the frequency of another amp-sine"
   (let [+ #'overtone.sc.ugen-collide/+]
     (amp-sine (+ hz1 (amp-sine hz2 amp2)) amp1)))
 
 (defsynth fm-perc [bus 0 note 60 length 0.25 mod-hz 100 mod-amp 200]
+  "FM percussion synth"
   (let [env (env-gen:kr (perc 0.03 length) :action FREE)
         fmop (fm-op (midicps note) 1.0 mod-hz mod-amp)]
     (out bus (* 0.7 env fmop))))
@@ -37,15 +40,21 @@
 
 (defonce fm-grp (group "OOTC Group"))
 
+(defn fm-loop [max-count]
+  (looper (metronome 240)
+              1
+              max-count
+              fm-perc
+              fm-grp
+              (shuffle [60 64 72 76 84 90])
+              (shuffle [0.1 0.3 0.6 0.2 1.0])
+              (shuffle [200 400 300 800 1200 2000 4100])
+              (shuffle [1200 800 900])))
+
+(defn insert-reverb []
+  (verb/schroederverb [:tail fm-grp] :bus 0 :decay 4.0))
+
 (defn fm-party [max-count]
   (do
-    (looper (metronome 240)
-            1
-            max-count
-            fm-perc
-            fm-grp
-            (shuffle [60 64 72 76 84 90])
-            (shuffle [0.1 0.3 0.6 0.2 1.0])
-            (shuffle [200 400 300 800 1200 2000 4100])
-            (shuffle [1200 800 900]))
-    (verb/schroederverb [:tail fm-grp] :bus 0 :decay 4.0)))
+    (fm-loop max-count)
+    (insert-reverb)))
