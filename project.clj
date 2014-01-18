b3ac608e7c597f6ee4bb55adef138c826e8e251d
Joshua Davison <josh@stringbot.com>
2014-01-18 17:49:05 -0600

    load the everything when the repl is started

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [overtone "0.9.1"]]
-  :main sbot-lib.core)
+  :main sbot-lib.main)
---
+++ src/sbot_lib/main.clj
@@ -0,0 +1,4 @@
+(ns sbot-lib.main
+  (:use [overtone.live]
+   :require [sbot-lib.reverb :as verb]
+   :require [sbot-lib.fm-ops :as fm]))
